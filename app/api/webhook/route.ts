397e7858457b5320ca5f212ad79ff0a38625b5a2
AdamullaOsas <kpasek22@gmail.com>
2024-04-06 14:05:49 +0200

    added console log

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -6,9 +6,8 @@ import { createUser, deleteUser, updateUser } from "@/lib/actions/user.action";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-    // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
     const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET;
-
+    console.log(WEBHOOK_SECRET);
     if (!WEBHOOK_SECRET) {
         throw new Error(
             "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
