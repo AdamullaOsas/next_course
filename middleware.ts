177fa55ed96a2008ec80d724a337fed42e99cfb1
AdamullaOsas <kpasek22@gmail.com>
2024-04-06 16:43:59 +0200

    adding routes

--- middleware.ts
+++ middleware.ts
@@ -10,6 +10,7 @@ export default authMiddleware({
         "/profile/:id",
         "/community",
         "/jobs",
+        "/.well-known/vercel/flags",
     ],
     ignoredRoutes: ["/api/webhook", "/api/chatgpt"],
 });
