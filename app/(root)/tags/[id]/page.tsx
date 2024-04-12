13617233b7a19dd737a836ece2e7b40fb842d20e
AdamullaOsas <kpasek22@gmail.com>
2024-04-12 20:39:15 +0200

    last errors

--- app/(root)/tags/[id]/page.tsx
+++ app/(root)/tags/[id]/page.tsx
@@ -3,7 +3,6 @@ import HomeFilters from "@/components/home/HomeFilters";
 import NoResult from "@/components/shared/NoResult/NoResult";
 import Pagination from "@/components/shared/Pagination/Pagination";
 import LocalSearch from "@/components/shared/search/LocalSearch";
-import { IQuestion } from "@/database/question.model";
 import { getQuestionsByTagId } from "@/lib/actions/tag.actions";
 import { URLProps } from "@/types";
 import React from "react";
@@ -30,7 +29,7 @@ const Page = async ({ params, searchParams }: URLProps) => {
             <HomeFilters />
             <div className="mt-10 flex w-full flex-col gap-6">
                 {result.questions.length > 0 ? (
-                    result.questions.map((question: IQuestion) => (
+                    result.questions.map((question: any) => (
                         <QuestionCard
                             key={question._id}
                             _id={question._id}
