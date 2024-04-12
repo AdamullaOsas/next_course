b7d975e6dc2c5992551ccaf96da950be3eeb3792
AdamullaOsas <kpasek22@gmail.com>
2024-04-12 17:33:42 +0200

    AI functionality

--- components/forms/Answer.tsx
+++ components/forms/Answer.tsx
@@ -1,12 +1,18 @@
 "use client";
 
-import React, { useRef, useState } from "react";
-import { Form, FormField, FormItem, FormMessage } from "../ui/form";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage,
+} from "../ui/form";
 import { useForm } from "react-hook-form";
 import { AnswerSchema } from "@/lib/validations";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Editor } from "@tinymce/tinymce-react";
+import { useRef, useState } from "react";
 import { useTheme } from "@/context/ThemeProvider";
 import { Button } from "../ui/button";
 import Image from "next/image";
@@ -20,11 +26,11 @@ interface Props {
 }
 
 const Answer = ({ question, questionId, authorId }: Props) => {
-    const [isSubbmitting, setisSubbmitting] = useState(false);
-    const [isSubbmittingAI, setisSubbmittingAI] = useState(false);
+    const pathname = usePathname();
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [isSubmittingAI, setIsSubmittingAI] = useState(false);
     const { mode } = useTheme();
     const editorRef = useRef(null);
-    const pathname = usePathname();
     const form = useForm<z.infer<typeof AnswerSchema>>({
         resolver: zodResolver(AnswerSchema),
         defaultValues: {
@@ -33,7 +39,7 @@ const Answer = ({ question, questionId, authorId }: Props) => {
     });
 
     const handleCreateAnswer = async (values: z.infer<typeof AnswerSchema>) => {
-        setisSubbmitting(true);
+        setIsSubmitting(true);
 
         try {
             await createAnswer({
@@ -46,20 +52,21 @@ const Answer = ({ question, questionId, authorId }: Props) => {
             form.reset();
 
             if (editorRef.current) {
-                // @ts-ignore
-                editorRef.current.setContent("");
+                const editor = editorRef.current as any;
+
+                editor.setContent("");
             }
         } catch (error) {
             console.log(error);
         } finally {
-            setisSubbmitting(false);
+            setIsSubmitting(false);
         }
     };
 
     const generateAIAnswer = async () => {
         if (!authorId) return;
 
-        setisSubbmittingAI(true);
+        setIsSubmittingAI(true);
 
         try {
             const response = await fetch(
@@ -69,36 +76,54 @@ const Answer = ({ question, questionId, authorId }: Props) => {
                     body: JSON.stringify({ question }),
                 }
             );
+
             const aiAnswer = await response.json();
 
-            alert(aiAnswer.reply);
+            // Convert plain text to HTML format
+
+            const formattedAnswer = aiAnswer.reply.replace(/\n/g, "<br />");
+
+            if (editorRef.current) {
+                const editor = editorRef.current as any;
+                editor.setContent(formattedAnswer);
+            }
+
+            // Toast...
         } catch (error) {
             console.log(error);
-            throw error;
         } finally {
-            setisSubbmittingAI(false);
+            setIsSubmittingAI(false);
         }
     };
+
     return (
         <div>
             <div className="flex flex-col justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
                 <h4 className="paragraph-semibold text-dark400_light800">
                     Write your answer here
                 </h4>
+
                 <Button
                     className="btn light-border-2 gap-1.5 rounded-md px-4 py-2.5 text-primary-500 shadow-none dark:text-primary-500"
                     onClick={generateAIAnswer}
                 >
-                    <Image
-                        src="/assets/icons/stars.svg"
-                        alt="star"
-                        width={12}
-                        height={12}
-                        className="object-contain"
-                    />
-                    Generate AI answer
+                    {isSubmittingAI ? (
+                        <>Generating...</>
+                    ) : (
+                        <>
+                            <Image
+                                src="/assets/icons/stars.svg"
+                                alt="star"
+                                width={12}
+                                height={12}
+                                className="object-contain"
+                            />
+                            Generate AI Answer
+                        </>
+                    )}
                 </Button>
             </div>
+
             <Form {...form}>
                 <form
                     className="mt-6 flex w-full flex-col gap-10"
@@ -109,65 +134,69 @@ const Answer = ({ question, questionId, authorId }: Props) => {
                         name="answer"
                         render={({ field }) => (
                             <FormItem className="flex w-full flex-col gap-3">
-                                <Editor
-                                    apiKey={
-                                        process.env
-                                            .NEXT_PUBLIC_TINY_EDITOR_API_KEY
-                                    }
-                                    onInit={(evt, editor) =>
-                                        // @ts-ignore
-                                        (editorRef.current = editor)
-                                    }
-                                    onBlur={field.onBlur}
-                                    onEditorChange={(content) =>
-                                        field.onChange(content)
-                                    }
-                                    init={{
-                                        height: 350,
-                                        menubar: false,
-                                        plugins: [
-                                            "advlist",
-                                            "autolink",
-                                            "lists",
-                                            "link",
-                                            "image",
-                                            "charmap",
-                                            "preview",
-                                            "anchor",
-                                            "searchreplace",
-                                            "visualblocks",
-                                            "codesample",
-                                            "fullscreen",
-                                            "insertdatetime",
-                                            "media",
-                                            "tables",
-                                        ],
-                                        toolbar:
-                                            "undo redo | " +
-                                            "codesample | bold italic forecolor | alignleft aligncenter | " +
-                                            "alignright alignjustify | bullist numlist",
-                                        content_style:
-                                            "body { font-family:Inter; font-size:16px }",
-                                        skin:
-                                            mode === "dark"
-                                                ? "oxide-dark"
-                                                : "oxide",
-                                        content_css:
-                                            mode === "dark" ? "dark" : "light",
-                                    }}
-                                />
-
+                                <FormControl className="mt-3.5">
+                                    <Editor
+                                        apiKey={
+                                            process.env
+                                                .NEXT_PUBLIC_TINY_EDITOR_API_KEY
+                                        }
+                                        onInit={(evt, editor) => {
+                                            // @ts-ignore
+                                            editorRef.current = editor;
+                                        }}
+                                        onBlur={field.onBlur}
+                                        onEditorChange={(content) =>
+                                            field.onChange(content)
+                                        }
+                                        init={{
+                                            height: 350,
+                                            menubar: false,
+                                            plugins: [
+                                                "advlist",
+                                                "autolink",
+                                                "lists",
+                                                "link",
+                                                "image",
+                                                "charmap",
+                                                "preview",
+                                                "anchor",
+                                                "searchreplace",
+                                                "visualblocks",
+                                                "codesample",
+                                                "fullscreen",
+                                                "insertdatetime",
+                                                "media",
+                                                "table",
+                                            ],
+                                            toolbar:
+                                                "undo redo | " +
+                                                "codesample | bold italic forecolor | alignleft aligncenter |" +
+                                                "alignright alignjustify | bullist numlist",
+                                            content_style:
+                                                "body { font-family:Inter; font-size:16px }",
+                                            skin:
+                                                mode === "dark"
+                                                    ? "oxide-dark"
+                                                    : "oxide",
+                                            content_css:
+                                                mode === "dark"
+                                                    ? "dark"
+                                                    : "light",
+                                        }}
+                                    />
+                                </FormControl>
                                 <FormMessage className="text-red-500" />
                             </FormItem>
                         )}
                     />
+
                     <div className="flex justify-end">
                         <Button
                             type="submit"
                             className="primary-gradient w-fit text-white"
-                            disabled={isSubbmitting}
+                            disabled={isSubmitting}
                         >
-                            {isSubbmitting ? "Submitting..." : "Submit"}
+                            {isSubmitting ? "Submitting..." : "Submit"}
                         </Button>
                     </div>
                 </form>
