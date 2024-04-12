c148386d6d3938ae15ccd4cd3933b7921b7ef578
AdamullaOsas <kpasek22@gmail.com>
2024-04-12 18:23:50 +0200

    question up/down vote and save toaster added

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import LeftSidebar from "@/components/shared/LeftSidebar/LeftSidebar";
 import RightSidebar from "@/components/shared/RightSidebar/RightSidebar";
 import Navbar from "@/components/shared/navbar/Navbar";
+import { Toaster } from "@/components/ui/toaster";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
@@ -14,7 +15,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
                 </section>
                 <RightSidebar />
             </div>
-            Toaster
+            <Toaster />
         </main>
     );
 };
